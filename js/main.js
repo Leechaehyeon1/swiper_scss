b22c449eef59666a41b2433fb020a2c72a193d1a
Leechaehyeon1 <lch09253@hanmail.net>
2021-10-09 11:27:33 +0900

    step5

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ var swiper = new Swiper('#wrap', {
   },
   loop: true,
   spaceBetween: 50,
-  speed: 500,
+  speed: 1000,
   slidesPerView: 'auto', // 지정한 넓이 값으로 적용
   centeredSlides: true, // 현재 활성화 패널을 항상 가로 가운데 배치
   effect: 'coverflow',
@@ -20,5 +20,23 @@ var swiper = new Swiper('#wrap', {
     slideShadows: false,
     depth: 400, // 3d 요소의 왜곡률
     stretch: -100,  // 주변 패널을 밀어냄, 양수 : 주변 패널을 당김
+  },
+  autoplay: {
+    delay: 2500,
   }
+});
+
+const btnStart = document.querySelector('.start-btn');
+const btnStop = document.querySelector('.stop-btn');
+
+btnStart.addEventListener('click', (e) => {
+  swiper.autoplay.start();
+  btnStart.classList.add('on');
+  btnStop.classList.remove('on');
+});
+
+btnStop.addEventListener('click', (e) => {
+  swiper.autoplay.stop();
+  btnStop.classList.add('on');
+  btnStart.classList.remove('on');
 });
